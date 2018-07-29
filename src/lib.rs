78dfff3256a7e838206f4fd55f4774926bf301ee
Jackson O'Donnell <jacksonhodonnell@gmail.com>
2018-07-29 13:49:07 -0500

    improved error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,113 @@
 extern crate libc;
 
+use std::convert;
+use std::error;
 use std::ffi::{CStr, CString};
+use std::fmt;
 use std::os::raw;
 
 mod bindings;
 pub use bindings::root::{DATABLOCK_STATUS, datablock_type_t};
 pub use bindings::root::__BindgenComplex as Complex;
 
+impl fmt::Display for DATABLOCK_STATUS {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl error::Error for DATABLOCK_STATUS {
+    fn description(&self) -> &str {
+        match *self {
+            DATABLOCK_STATUS::DBS_SUCCESS => "DBS_SUCCESS",
+            DATABLOCK_STATUS::DBS_DATABLOCK_NULL => "DBS_DATABLOCK_NULL",
+            DATABLOCK_STATUS::DBS_SECTION_NULL => "DBS_SECTION_NULL",
+            DATABLOCK_STATUS::DBS_SECTION_NOT_FOUND => "DBS_SECTION_NOT_FOUND",
+            DATABLOCK_STATUS::DBS_NAME_NULL => "DBS_NAME_NULL",
+            DATABLOCK_STATUS::DBS_NAME_NOT_FOUND => "DBS_NAME_NOT_FOUND",
+            DATABLOCK_STATUS::DBS_NAME_ALREADY_EXISTS => "DBS_NAME_ALREADY_EXISTS",
+            DATABLOCK_STATUS::DBS_VALUE_NULL => "DBS_VALUE_NULL",
+            DATABLOCK_STATUS::DBS_WRONG_VALUE_TYPE => "DBS_WRONG_VALUE_TYPE",
+            DATABLOCK_STATUS::DBS_MEMORY_ALLOC_FAILURE => "DBS_MEMORY_ALLOC_FAILURE",
+            DATABLOCK_STATUS::DBS_SIZE_NULL => "DBS_SIZE_NULL",
+            DATABLOCK_STATUS::DBS_SIZE_NONPOSITIVE => "DBS_SIZE_NONPOSITIVE",
+            DATABLOCK_STATUS::DBS_SIZE_INSUFFICIENT => "DBS_SIZE_INSUFFICIENT",
+            DATABLOCK_STATUS::DBS_NDIM_NONPOSITIVE => "DBS_NDIM_NONPOSITIVE",
+            DATABLOCK_STATUS::DBS_NDIM_OVERFLOW => "DBS_NDIM_OVERFLOW",
+            DATABLOCK_STATUS::DBS_NDIM_MISMATCH => "DBS_NDIM_MISMATCH",
+            DATABLOCK_STATUS::DBS_EXTENTS_NULL => "DBS_EXTENTS_NULL",
+            DATABLOCK_STATUS::DBS_EXTENTS_MISMATCH => "DBS_EXTENTS_MISMATCH",
+            DATABLOCK_STATUS::DBS_LOGIC_ERROR => "DBS_LOGIC_ERROR",
+            DATABLOCK_STATUS::DBS_USED_DEFAULT => "DBS_USED_DEFAULT"
+        }
+    }
+}
 
 #[derive(Debug)]
+/// Error type for CosmoSIS. Wraps a `DATABLOCK_STATUS` and contains an optional
+/// error message.
 pub struct CosmosisError {
     pub kind: DATABLOCK_STATUS,
-    pub reason: Option<String>
+    reason: Option<String>
+}
+
+impl fmt::Display for CosmosisError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "CosmosisError")?;
+        if self.kind == DATABLOCK_STATUS::DBS_SUCCESS {
+            write!(f, "(success)")
+        } else {
+            write!(f, "(error: {}", error::Error::description(&self.kind))?;
+            if self.reason.is_some() {
+                write!(f, ", reason: {})", self.reason.as_ref().unwrap())
+            } else {
+                write!(f, ")")
+            }
+        }
+    }
+}
+
+impl error::Error for CosmosisError {
+    fn description(&self) -> &str {
+        if self.reason.is_some() {
+            self.reason.as_ref().map(|s| &*s).unwrap()
+        } else {
+            self.kind.description()
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        Some(&self.kind)
+    }
+}
+
+impl convert::From<DATABLOCK_STATUS> for CosmosisError {
+    fn from(obj: DATABLOCK_STATUS) -> Self {
+        CosmosisError { kind: obj, reason: None }
+    }
 }
 
 pub type CosmosisResult<T> = Result<T, CosmosisError>;
 
 macro_rules! wrap_cosmosis_result {
-    ( $obj:expr, $err:expr ) => {
+    ( $err:expr, $obj:expr ) => {
         if $err == DATABLOCK_STATUS::DBS_SUCCESS {
             Ok($obj)
         } else {
             Err(CosmosisError { kind: $err, reason: None })
         }
+    };
+    ( $err:expr, $obj:expr, $( $fmt_arg:expr ),* ) => {
+        if $err == DATABLOCK_STATUS::DBS_SUCCESS {
+            Ok($obj)
+        } else {
+            Err(CosmosisError { kind: $err, reason: Some(format!($( $fmt_arg ),*)) })
+        }
     }
 }
 
+/// CosmoSIS Data Storage block, all input parameters and outputs are passed through
+/// DataBlocks.
 pub struct DataBlock {
     ptr: *mut bindings::root::c_datablock
 }
@@ -88,6 +170,7 @@ impl DataBlock {
     }
 }
 
+/// Types which can be stored and retrieved from `DataBlock`s are `CosmosisDataType`s.
 pub trait CosmosisDataType: Sized {
     fn cosmosis_type() -> datablock_type_t;
     fn get_datablock(&mut DataBlock, section: &str, name: &str) -> CosmosisResult<Self>;
@@ -105,23 +188,26 @@ macro_rules! gen_cosmosis_data_type {
 
             fn get_datablock(db: &mut DataBlock, section: &str, name: &str) -> CosmosisResult<Self> {
                 let mut n: Self = $default_val;
-                let result = unsafe {
+                let retval = unsafe {
                     $getter(db.ptr,
                             CString::new(section).unwrap().as_ptr(),
                             CString::new(name).unwrap().as_ptr(),
                             &mut n)
                 };
-                wrap_cosmosis_result!(n, result)
+                wrap_cosmosis_result!(retval, n, "Could not get value at (section, name): ({}, {})",
+                                      section, name)
             }
 
             fn put_datablock(db: &mut DataBlock, section: &str, name: &str, obj: Self) -> CosmosisResult<()> {
-                let result = unsafe {
+                let retval = unsafe {
                     $putter(db.ptr,
                             CString::new(section).unwrap().as_ptr(),
                             CString::new(name).unwrap().as_ptr(),
                             obj)
                 };
-                wrap_cosmosis_result!((), result)
+                wrap_cosmosis_result!(retval, (), "Could not put value at (section, name): ({}, {})",
+                                      section, name)
+
             }
         }
     }
@@ -147,13 +233,13 @@ impl CosmosisDataType for CString {
 
     fn get_datablock(db: &mut DataBlock, section: &str, name: &str) -> CosmosisResult<Self> {
         let mut cstr: *mut raw::c_char = std::ptr::null_mut();
-        let result = unsafe {
+        let retval = unsafe {
             bindings::root::c_datablock_get_string(db.ptr,
                                                    CString::new(section).unwrap().as_ptr(),
                                                    CString::new(name).unwrap().as_ptr(),
                                                    &mut cstr)
         };
-        wrap_cosmosis_result!(
+        wrap_cosmosis_result!(retval, 
             unsafe {
                 let cstr_ref = CStr::from_ptr(cstr);
                 // Yes, this would be an unnecessary allocation, but we must clone
@@ -164,23 +250,24 @@ impl CosmosisDataType for CString {
                 libc::free(cstr as *mut libc::c_void);
                 output_string
             },
-            result)
+            "Could not get value at (section, name): ({}, {})", section, name)
     }
 
     fn put_datablock(db: &mut DataBlock, section: &str, name: &str, obj: Self) -> CosmosisResult<()> {
-        let result = unsafe {
+        let retval = unsafe {
             bindings::root::c_datablock_put_string(db.ptr,
                                                    CString::new(section).unwrap().as_ptr(),
                                                    CString::new(name).unwrap().as_ptr(),
                                                    obj.as_ptr())
         };
-        wrap_cosmosis_result!((), result)
+        wrap_cosmosis_result!(retval, (), "Could not put value at (section, name): ({}, {})",
+                              section, name)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{DataBlock, DATABLOCK_STATUS};
+    use super::{DataBlock, DATABLOCK_STATUS, datablock_type_t};
     use std::os::raw;
 
     #[test]
@@ -217,6 +304,8 @@ mod tests {
         for (name, _) in numbers.iter() {
             assert!(db.get::<raw::c_int>("my_section", name).unwrap_err().kind
                     == DATABLOCK_STATUS::DBS_WRONG_VALUE_TYPE);
+            assert!(db.get_type("my_section", name).unwrap()
+                    == datablock_type_t::DBT_DOUBLE);
         }
     }
 }
